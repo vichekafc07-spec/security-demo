41767c41f516286dfd6603e0a70df5815a87b8d2
Vichika <vichekafc07@gmail.com>
2026-01-13 14:47:27 +0700

    Delete by developer

--- src/main/java/com/library/spring_security/config/service/CustomUserDetails.java
+++ src/main/java/com/library/spring_security/config/service/CustomUserDetails.java
@@ -1,74 +1,51 @@
 package com.library.spring_security.config.service;
 
-import com.library.spring_security.domain.model.User;
-import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import java.util.Collection;
 
-@Getter
+import java.util.Collection;
+import java.util.List;
+
 public class CustomUserDetails implements UserDetails {
 
-    private final String email;
-    private final String password;
-    private final boolean enableds;
-    private final Collection<? extends GrantedAuthority> authorities;
+    private String email;
+    private String password;
+    private boolean enables;
 
-    private CustomUserDetails(
-                              String email,
-                              String password,
-                              boolean enabled,
-                              Collection<? extends GrantedAuthority> authorities) {
-        this.email = email;
-        this.password = password;
-        this.enableds = enabled;
-        this.authorities = authorities;
-    }
-    public static CustomUserDetails from(User user){
-        return new CustomUserDetails(
-                user.getEmail(),
-                user.getPassword(),
-                user.isEnabled(),
-                user.getRoles()
-                        .stream()
-                        .map(role -> new  SimpleGrantedAuthority("ROLE_" + role.getName()))
-                        .toList()
-        );
-    }
+
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
+        return List.of();
     }
 
     @Override
     public String getPassword() {
-        return password;
+        return "";
     }
 
     @Override
     public String getUsername() {
-        return email;
+        return "";
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return true;
+        return UserDetails.super.isAccountNonExpired();
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return true;
+        return UserDetails.super.isAccountNonLocked();
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return true;
+        return UserDetails.super.isCredentialsNonExpired();
     }
 
     @Override
     public boolean isEnabled() {
-        return isEnableds();
+        return UserDetails.super.isEnabled();
     }
 }
